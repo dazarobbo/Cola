bb3d527d20b5aec625a55ab91fc62d4e97955646
dazarobbo <_pm_me_on_bnet@bungie.net>
2015-06-20 20:41:11 +1000

    added iequatable

---
+++ src/IEquatable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Cola;
+
+/**
+ * IEquatable
+ */
+interface IEquatable {
+
+	/**
+	 * Returns whether this object equals another
+	 * @param mixed $obj
+	 * @return bool
+	 */
+	public function equals($obj);
+
+}
