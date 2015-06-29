8c6d202098cccfcb703b18257daa479d8f12c723
dazarobbo <_pm_me_on_bnet@bungie.net>
2015-06-29 22:33:44 +1000

    fixed namespace bug

--- src/Exceptions/DeprecatedException.php
+++ src/Exceptions/DeprecatedException.php
@@ -1,12 +1,9 @@
 <?php
 
-namespace Exceptions;
+namespace Cola\Exceptions;
 
 /**
  * DeprecatedException
  */
 class DeprecatedException extends \Exception {
-	public function __construct($message, $code = 0, \Exception $previous = null) {
-		parent::__construct($message, $code, $previous);
-	}
 }
